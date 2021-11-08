e4abca023fb434609135b4e7b6ee0b69c5761997
Rafael Cardoso <rrafa203@gmail.com>
2021-11-08 15:25:47 -0800

    add inquirer library

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
+const inquierer = require("inquirer");
 
-const generatePage = require("./src/page-template");
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
 
-const profileDataArgs = process.argv.slice(2);
+        }
+    ])
+    .then(answers => console.log(answers));
 
-// console.log(profileDataArgs);
+// const fs = require("fs");
 
+// const generatePage = require("./src/page-template");
 
+// const pageHTML = reneratePage(name, github);
 
-// const printProfileData = profileDataArr => {
-//     //This...
-//     for (let i=0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//     }
-//     console.log('==============');
+// fs.writeFile("index.html", pageHTML, err => {
+//    if(err) throw err;
 
-//     //Is the same as this
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem)
-//     });
-// };
-// printProfileData(profileDataArgs);
-
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// console.log(generatePage('Rafael', "janehub"));
-
-
-//console.log(generatePage('Rafael', "janehub"));
-
-//const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// console.log(generatePage(name, github));
-
-const [name, github] = profileDataArgs;
-//console.log(name, github);
-//console.log(generatePage(name, github));
-
-
-
-fs.writeFile("index.html", generatePage(name, github), err => {
-   if(err) throw err;
-
-   console.log("Portfolio complete! Checkout index.html to see the output!")
-}
-)
+//    console.log("Portfolio complete! Checkout index.html to see the output!")
+// });
